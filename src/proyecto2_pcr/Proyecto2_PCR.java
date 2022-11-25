1cdec5a157f030fde4926f3fd0b0a82e9e55ff65
Sebasr22 <r.sebastian2020@correo.unimet.edu.ve>
2022-11-25 10:13:38 -0400

    2do commit

--- src/proyecto2_pcr/Proyecto2_PCR.java
+++ src/proyecto2_pcr/Proyecto2_PCR.java
@@ -14,7 +14,8 @@ public class Proyecto2_PCR {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        interfazInicial ventana = new interfazInicial();
+        ventana.setVisible(true);
     }
-    
+
 }
